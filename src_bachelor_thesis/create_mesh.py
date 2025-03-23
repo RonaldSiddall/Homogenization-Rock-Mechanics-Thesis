3d7bacb2ecdab15474e47dfd69591ccfe95ac8eb
Ronald Siddall <ronald.christopher.siddall>
2025-03-24 00:42:22 +0100

    Fixed missing validation of mesh config parameters

--- src_bachelor_thesis/create_mesh.py
+++ src_bachelor_thesis/create_mesh.py
@@ -4,23 +4,26 @@ import time
 from Logic_classes.GenerateFractures import GenerateFractures
 from Logic_classes.GenerateMesh import GenerateMesh
 from Utility_methods.parameter_summaries import parameters_short_summary_print_out_mesh
+from Utility_methods.check_values_in_config_file import check_values_in_config_file_mesh
 
 # This function generates a mesh (.msh file) using the provided configuration file (config_file.yaml)
 def create_mesh(config_file):
-    try:
-        start_time = time.time()
-        fracture_set = GenerateFractures(config_file).generator_of_fracture_set()
-        GenerateMesh(config_file, fracture_set).make_mesh()
-        end_time = time.time()
-        name_of_script_mesh = os.path.basename(__file__)
-        parameters_short_summary_print_out_mesh(config_file, fracture_set, start_time, end_time, name_of_script_mesh)
-    except Exception as error_mess:
-        # If an error occurs during the execution, this prints the error message
-        print("-------------------------------------------------------------------------------")
-        print("\nAn error occurred during execution. The error message is written below:\n")
-        print(str(error_mess) + "\n")
-        print("-------------------------------------------------------------------------------")
-
+    # Checks the relevant values of the parameters used to create the .msh file
+    # If at least one parameter is invalid (False is returned) then method stops immediately
+    if check_values_in_config_file_mesh(config_file):
+        try:
+            start_time = time.time()
+            fracture_set = GenerateFractures(config_file).generator_of_fracture_set()
+            GenerateMesh(config_file, fracture_set).make_mesh()
+            end_time = time.time()
+            name_of_script_mesh = os.path.basename(__file__)
+            parameters_short_summary_print_out_mesh(config_file, fracture_set, start_time, end_time, name_of_script_mesh)
+        except Exception as error_mess:
+            # If an error occurs during the execution, this prints the error message
+            print("-------------------------------------------------------------------------------")
+            print("\nAn error occurred during execution. The error message is written below:\n")
+            print(str(error_mess) + "\n")
+            print("-------------------------------------------------------------------------------")
 
 if __name__ == "__main__":
     try:
