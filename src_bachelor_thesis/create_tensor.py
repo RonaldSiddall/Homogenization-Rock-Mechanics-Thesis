e4e27f108588e17181f05853aa80a28fe3be3880
Ronald Siddall <ronald.christopher.siddall@tul.cz>
2025-04-19 03:27:19 +0200

    Updated create tensor method

--- src_bachelor_thesis/create_tensor.py
+++ src_bachelor_thesis/create_tensor.py
@@ -16,6 +16,23 @@ def create_file_with_tensor(config_file, yaml_file):
         # This extracts the name of the file you are reading now
         name_of_script_flow = os.path.basename(__file__)
         parameters_short_summary_print_out_flow(config_file, yaml_file, start_time, end_time, name_of_script_flow)
+
+        # There wasn't enough time to implement an in-built function for writing the tensor to a file
+        # Which is why this quick-solve method is commented out... - but it works
+        # coefficients = GenerateTensor(config_file, yaml_file, vtu_dirs).compute_effect_elast_constants_voigt()
+        # c1 = coefficients[0][0]
+        # c2 = coefficients[0][1]
+        # c3 = coefficients[0][2]
+        # c4 = coefficients[1][0]
+        # c5 = coefficients[1][1]
+        # c6 = coefficients[1][2]
+        # c7 = coefficients[2][0]
+        # c8 = coefficients[2][1]
+        # c9 = coefficients[2][2]
+        # constants = [c1, c2, c3, c4, c5, c6, c7, c8, c9]
+        # with open("C:/Plocha/Bachelor_thesis/Python_scripts/Results_of_analysis/Results_geometry/all_cases_partial_results/case_10/all_coefficients_case_10.txt", "a") as file:
+        #    for constant in constants:
+        #        file.write(f"{constant}\n")
     except Exception as error_mess:
         # If an error occurs during the execution, this prints the error message
         print("-------------------------------------------------------------------------------")
